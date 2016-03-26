ed126a7c67fd74583a1e7640e02baf4f6d83cb2a
miaolapd <miaolapd@163.com>
2016-03-26 21:59:50 +0800

    添加获取网易云音乐外链地址的PHP脚本

---
+++ music163.php
@@ -0,0 +1,40 @@
+<?php
+// 获取网易云音乐外链地址
+
+function netease_http($url) {
+    $refer = "http://music.163.com/";
+    $header[] = "Cookie: " . "appver=1.5.0.75771;";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+    curl_setopt($ch, CURLOPT_REFERER, $refer);
+    $cexecute = curl_exec($ch);
+    curl_close($ch);
+    if ($cexecute) {
+        $result = json_decode($cexecute, true);
+        return $result;
+    } else {
+        return false;
+    }
+}
+
+function netease_song($music_id) {
+    $url = "http://music.163.com/api/song/detail/?id=" . $music_id . "&ids=%5B" . $music_id . "%5D";
+    $response = netease_http($url);
+    if ($response["code"] == 200 && $response["songs"]) {
+        //print_r($response["songs"]);
+        return str_replace("http://m", "http://p", $response["songs"][0]["mp3Url"]);
+    }
+    return false;
+}
+
+$id = $_REQUEST['id'];
+$url = netease_song($id);
+if ($url) {
+    Header("Location: $url");
+} else {
+    echo 'Music No Found';
+}
+?>
