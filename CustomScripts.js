0a5c3a33bd254a7f6a6f670b323271c4eb6a7c3a
miaolapd <miaolapd@163.com>
2016-04-14 23:19:14 +0800

    增加了【发帖常用文本】的自定义脚本

--- CustomScripts.js
+++ CustomScripts.js
@@ -937,3 +937,58 @@
 }());
 
 /*==========================================*/
+
+// 发帖常用文本 V1.0
+(function () {
+    if (location.pathname !== '/read.php' && location.pathname !== '/post.php') return;
+
+    /*
+     常用文本列表
+     可在此增删自定义的常用文本，有三种定义方式：
+     1. 只在option标签内包含文本的项目：
+     常用文本列表框和发帖框里最终显示均为option标签内的文本，例：'<option>常用文本1</option>'
+     2. 在value属性里定义了文本的项目：
+     option标签内的文本会显示在常用文本列表框内，而发帖框里最终显示的将是value属性里的文本
+     例：'<option value="这里是常用文本2！">常用文本2</option>'
+     3. 定义了data-action的项目：
+     option标签内的文本会显示在常用文本列表框内，而发帖框里最终显示的将是经过（动作列表中与data-action相匹配的）自定义函数所处理过的文本
+     例：'<option data-action="动作标签">常用文本3...</option>'
+     */
+    var textList = [
+        '<option data-action="红包">快捷红包...</option>',
+        '<option>感谢楼主的分享！</option>',
+        '<option value="恭喜楼主！[s:44]">恭喜楼主！</option>',
+    ];
+
+    // 动作列表：用于定义常用文本列表中与data-action属性相匹配的自定义函数，返回经过自定义函数处理的字符串
+    var actionList = {
+        '默认': function () {
+            return '';
+        },
+        '红包': function () {
+            var kfb = parseInt(window.prompt('请输入价格：', 100));
+            return !isNaN(kfb) ? '[sell={0}]感谢楼主的红包！[/sell]'.replace('{0}', kfb) : '';
+        },
+    };
+
+    $('<select id="a" style="width:110px;margin-left:10px"><option data-action="默认" selected="selected">发帖常用文本</option>' + textList.join('') + '</select>')
+        .insertAfter(location.pathname === '/read.php' ? 'input[name="Submit"]' : 'input[name="diy_guanjianci"]')
+        .change(function () {
+            var $selectItem = $(this.selectedOptions[0]);
+            var text = '';
+            var action = $selectItem.data('action');
+            if (action) {
+                if (typeof actionList[action] === 'function') text = actionList[action]();
+            }
+            else {
+                text = $selectItem.val();
+            }
+            if (text) {
+                var $textArea = $(location.pathname === '/read.php' ? 'textArea[name="atc_content"]' : '#textarea');
+                var content = $textArea.val();
+                $textArea.val(content ? content + '\n' + text : text).focus();
+            }
+        }).find('option[data-action]').not(':first-child').css('color', '#F00');
+}());
+
+/*==========================================*/
