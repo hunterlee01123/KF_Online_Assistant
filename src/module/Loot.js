2bf07de1f7f114e50e0ddcbe7182d928a06260be
miaolapd <miaolapd@163.com>
2016-11-15 19:17:41 +0800

    增加了直接修改争夺属性数值的功能

--- src/module/Loot.js
+++ src/module/Loot.js
@@ -2,64 +2,119 @@
 'use strict';
 import * as Item from './Item';
 
+// 争夺首页区域
+let $lootArea;
+// 争夺属性区域
+let $properties;
+// 属性点区域
+let $points;
+// 当前争夺属性
+let propertyList;
+// 附加属性点列表
+let extraPointList;
+// 道具使用情况
+let itemUsedNumList;
+
 /**
  * 增强争夺首页
  */
 export const enhanceLootIndexPage = function () {
-    let $area = $('.kf_fw_ig1');
-    let $properties = $area.find('> tbody > tr:nth-child(3) > td:first-child');
-    let $points = $properties.next('td');
-    let propertyList = getCurrentLootPropertyList();
-    let itemUsedNumList = Item.getItemUsedInfo($area.find('> tbody > tr:nth-child(4) > td').html());
+    $lootArea = $('.kf_fw_ig1:first');
+    $properties = $lootArea.find('> tbody > tr:nth-child(3) > td:first-child');
+    $points = $lootArea.find('> tbody > tr:nth-child(3) > td:nth-child(2)');
+    propertyList = getLootPropertyList();
+    extraPointList = getExtraPointList();
+    itemUsedNumList = Item.getItemUsedInfo($lootArea.find('> tbody > tr:nth-child(4) > td').html());
+    handlePropertiesArea();
+    handlePointsArea();
+    enhanceLootLog();
+};
 
-    $properties.html(
-        $properties.html().replace(
-            '技能伤害：攻击伤害+(体质点数*4)',
-            '技能伤害：<span class="pd_custom_tips" id="pd_skill_attack" title="攻击伤害+(体质点数*4)"></span>'
-        )
-    );
-    $properties.find('br').each(function (index) {
-        let name = '';
-        switch (index) {
-            case 1:
-                name = 's1';
-                break;
-            case 2:
-                name = 's2';
-                break;
-            case 3:
-                name = 'd1';
-                break;
-            case 4:
-                name = 'd2';
-                break;
-            case 6:
-                name = 'i1';
-                break;
-            case 7:
-                name = 'i2';
-                break;
-        }
-        if (name) {
-            $(this).before(` <span style="color:#777" id="pd_new_${name}"></span>`);
-        }
-    });
+/**
+ * 处理争夺属性区域
+ */
+const handlePropertiesArea = function () {
+    let html = $properties.html()
+        .replace(/(攻击力：)(\d+)/, '$1<span id="pdPro_s1" title="原值：$2">$2</span> <span id="pdNew_s1"></span>')
+        .replace(/(生命值：\d+)\s*\(最大(\d+)\)/, '$1 (最大<span id="pdPro_s2" title="原值：$2">$2</span>) <span id="pdNew_s2"></span>')
+        .replace(/(攻击速度：)(\d+)/, '$1<span id="pdPro_d1" title="原值：$2">$2</span> <span id="pdNew_d1"></span>')
+        .replace(/(暴击几率：)(\d+)%/, '$1<span id="pdPro_d2" title="原值：$2">$2</span>% <span id="pdNew_d2"></span>')
+        .replace(/(技能释放概率：)(\d+)%/, '$1<span id="pdPro_i1" title="原值：$2">$2</span>% <span id="pdNew_i1"></span>')
+        .replace(/(防御：)(\d+)%减伤/, '$1<span id="pdPro_i2" title="原值：$2">$2</span>%减伤 <span id="pdNew_i2"></span>')
+        .replace('技能伤害：攻击伤害+(体质点数*4)', '技能伤害：<span class="pd_custom_tips" id="pdSkillAttack" title="攻击伤害+(体质点数*4)"></span>');
+    $properties.html(html);
 
+    $properties.on('click', '[id^="pdPro_"]', function () {
+        let $this = $(this);
+        $this.hide();
+        let name = $this.attr('id').replace('pdPro_', '');
+        let step = 1;
+        if (name === 's1') step = 5;
+        else if (name === 's2') step = 20;
+        else if (name === 'd1') step = 2;
+        $(`<input data-name="${name}" type="number" value="${parseInt($this.text())}" min="1" step="${step}" ` +
+            `style="width: 65px; margin-right: 5px;" title="${$this.attr('title')}">`
+        ).insertAfter($this)
+            .focus()
+            .select()
+            .blur(function () {
+                let $this = $(this);
+                let name = $this.data('name');
+                let num = parseInt($this.val());
+                if (num > 0) {
+                    let newValue = 0;
+                    switch (getPointNameByFieldName(name)) {
+                        case '力量':
+                            newValue = Math.round(num / 5) - extraPointList.get('力量');
+                            break;
+                        case '体质':
+                            newValue = Math.round((itemUsedNumList.get('蕾米莉亚同人漫画') === 50 ? num - 700 : num) / 20) - extraPointList.get('体质');
+                            break;
+                        case '敏捷':
+                            newValue = Math.round((itemUsedNumList.get('十六夜同人漫画') === 50 ? num - 100 : num) / 2) - extraPointList.get('敏捷');
+                            break;
+                        case '灵活':
+                            newValue = Math.round(100 * num / (100 - num)) - extraPointList.get('灵活');
+                            break;
+                        case '智力':
+                            newValue = Math.round(120 * num / (100 - num)) - extraPointList.get('智力');
+                            break;
+                        case '意志':
+                            newValue = Math.round(150 * num / (100 - num)) - extraPointList.get('意志');
+                            break;
+                    }
+                    if (!isFinite(newValue) || newValue <= 0) newValue = 1;
+                    $points.find(`[name="${name}"]`).val(newValue).trigger('change');
+                }
+                $this.prev().show().end().remove();
+            })
+            .keyup(function (e) {
+                let $this = $(this);
+                if (e.keyCode === 13) $this.blur();
+                else if (e.keyCode === 27) $this.val('').blur();
+            });
+    }).find('[id^=pdPro_]').css('cursor', 'pointer');
+};
+
+/**
+ * 处理属性点区域
+ */
+const handlePointsArea = function () {
     $points.find('[type="text"]').attr('type', 'number').attr('min', 1).attr('max', 999).prop('required', true).css('width', '60px');
     $points.find('input[readonly]').attr('min', 0).prop('disabled', true).removeProp('required', true);
-    $points.prepend('<span class="pd_highlight">剩余属性点：<span id="pd_surplus_point"></span></span><br>');
+    $points.prepend('<span class="pd_highlight">剩余属性点：<span id="pdSurplusPoint"></span></span><br>');
 
     $points.on('change', '[type="number"]', function () {
         let $this = $(this);
-        $('#pd_surplus_point').text(propertyList.get('可分配属性点') - getCurrentAssignedPoint());
-        showNewLootProperty($this, propertyList, itemUsedNumList);
+        $('#pdSurplusPoint').text(propertyList.get('可分配属性点') - getCurrentAssignedPoint());
+        showNewLootProperty($this);
         showSumOfPoint($this);
 
         let skillAttack = 0;
-        let matches = /\d+/.exec($area.find('[name="s1"]').next('span').next('.pd_point_sum').text());
+        let matches = /\d+/.exec($lootArea.find('[name="s1"]').next('span').next('.pd_point_sum').text());
         if (matches) skillAttack = parseInt(matches[0]) * 5;
-        skillAttack += parseInt($area.find('[name="s2"]').val()) * 4;
-        $('#pd_skill_attack').text(skillAttack);
+        skillAttack += parseInt($lootArea.find('[name="s2"]').val()) * 4;
+        $('#pdSkillAttack').text(skillAttack);
     }).on('click', '.pd_point_sum', function () {
         let surplusPoint = propertyList.get('可分配属性点') - getCurrentAssignedPoint();
         if (!surplusPoint) return;
@@ -77,8 +132,38 @@ export const enhanceLootIndexPage = function () {
             return confirm('你的可分配属性点尚未用完，是否提交？');
         }
     }).find('[type="number"]').trigger('change');
+};
 
-    enhanceLootLog();
+/**
+ * 获取争夺属性列表
+ * @returns {Map} 争夺属性
+ */
+const getLootPropertyList = function () {
+    let propertyList = new Map([
+        ['攻击力', 0],
+        ['最大生命值', 0],
+        ['攻击速度', 0],
+        ['暴击几率', 0],
+        ['技能释放概率', 0],
+        ['防御', 0],
+        ['可分配属性点', 0],
+    ]);
+    let html = $properties.html();
+    let matches = /攻击力：(\d+)/.exec(html);
+    if (matches) propertyList.set('攻击力', parseInt(matches[1]));
+    matches = /生命值：\d+\s*\(最大(\d+)\)/.exec(html);
+    if (matches) propertyList.set('最大生命值', parseInt(matches[1]));
+    matches = /攻击速度：(\d+)/.exec(html);
+    if (matches) propertyList.set('攻击速度', parseInt(matches[1]));
+    matches = /暴击几率：(\d+)%/.exec(html);
+    if (matches) propertyList.set('暴击几率', parseInt(matches[1]));
+    matches = /技能释放概率：(\d+)%/.exec(html);
+    if (matches) propertyList.set('技能释放概率', parseInt(matches[1]));
+    matches = /防御：(\d+)%/.exec(html);
+    if (matches) propertyList.set('防御', parseInt(matches[1]));
+    matches = /可分配属性点：(\d+)/.exec(html);
+    if (matches) propertyList.set('可分配属性点', parseInt(matches[1]));
+    return propertyList;
 };
 
 /**
@@ -87,7 +172,7 @@ export const enhanceLootIndexPage = function () {
  */
 const getCurrentAssignedPoint = function () {
     let usedPoint = 0;
-    $('.kf_fw_ig1').find('[type="number"]').each(function () {
+    $points.find('[type="number"]').each(function () {
         let point = parseInt($(this).val());
         if (point && point > 0) usedPoint += point;
     });
@@ -96,6 +181,7 @@ const getCurrentAssignedPoint = function () {
 
 /**
  * 显示各项属性点的和值
+ * @param {jQuery} $point 属性点字段对象
  */
 const showSumOfPoint = function ($point) {
     let num = parseInt($point.val());
@@ -110,50 +196,71 @@ const showSumOfPoint = function ($point) {
 };
 
 /**
- * 获取当前的争夺属性
- * @returns {Map} 争夺属性
+ * 获取附加属性点列表
+ * @returns {Map} 附加属性点列表
  */
-const getCurrentLootPropertyList = function () {
-    let propertyList = new Map([
-        ['攻击力', 0],
-        ['最大生命值', 0],
-        ['攻击速度', 0],
-        ['暴击几率', 0],
-        ['技能释放概率', 0],
-        ['防御', 0],
-        ['可分配属性点', 0],
+const getExtraPointList = function () {
+    let extraPointList = new Map([
+        ['力量', 0],
+        ['体质', 0],
+        ['敏捷', 0],
+        ['灵活', 0],
+        ['智力', 0],
+        ['意志', 0],
+        ['耐力', 0],
+        ['幸运', 0],
     ]);
-    let html = $('.kf_fw_ig1 > tbody > tr:nth-child(3) > td:first-child').html();
-    let matches = /攻击力：(\d+)/.exec(html);
-    if (matches) propertyList.set('攻击力',  parseInt(matches[1]));
-    matches = /生命值：\d+\s*\(最大(\d+)\)/.exec(html);
-    if (matches) propertyList.set('最大生命值',  parseInt(matches[1]));
-    matches = /攻击速度：(\d+)/.exec(html);
-    if (matches) propertyList.set('攻击速度',  parseInt(matches[1]));
-    matches = /暴击几率：(\d+)%/.exec(html);
-    if (matches) propertyList.set('暴击几率',  parseInt(matches[1]));
-    matches = /技能释放概率：(\d+)%/.exec(html);
-    if (matches) propertyList.set('技能释放概率',  parseInt(matches[1]));
-    matches = /防御：(\d+)%/.exec(html);
-    if (matches) propertyList.set('防御',  parseInt(matches[1]));
-    matches = /可分配属性点：(\d+)/.exec(html);
-    if (matches) propertyList.set('可分配属性点',  parseInt(matches[1]));
-    return propertyList;
+    $points.find('[type="text"]').each(function () {
+        let $this = $(this);
+        let name = $this.attr('name');
+        let num = parseInt($this.next('span').text());
+        let key = getPointNameByFieldName(name);
+        if (!isNaN(num) && key) {
+            extraPointList.set(key, num);
+        }
+    });
+    return extraPointList;
+};
+
+/**
+ * 根据字段名称获取属性点名称
+ * @param {string} fieldName 字段名称
+ * @returns {string} 属性点名称
+ */
+const getPointNameByFieldName = function (fieldName) {
+    switch (fieldName) {
+        case 's1':
+            return '力量';
+        case 's2':
+            return '体质';
+        case 'd1':
+            return '敏捷';
+        case 'd2':
+            return '灵活';
+        case 'i1':
+            return '智力';
+        case 'i2':
+            return '意志';
+        case 'p':
+            return '耐力';
+        case 'l':
+            return '幸运';
+        default:
+            return '';
+    }
 };
 
 /**
  * 显示新的争夺属性
- * @param {jQuery} $point 属性字段
- * @param {Map} propertyList 当前的争夺属性
- * @param {Map} itemUsedNumList 道具使用情况对象
+ * @param {jQuery} $point 属性点字段对象
  */
-const showNewLootProperty = function ($point, propertyList, itemUsedNumList) {
+const showNewLootProperty = function ($point) {
     let name = $point.attr('name');
     let num = parseInt($point.val());
     if (isNaN(num) || num < 0) num = 0;
     let oriNum = parseInt($point.get(0).defaultValue);
     let extraNum = parseInt($point.next('span').text());
-    let newValue = 0, diffValue = 0, unit = '';
+    let newValue = 0, diffValue = 0;
     switch (name) {
         case 's1':
             newValue = (num + extraNum) * 5;
@@ -171,37 +278,31 @@ const showNewLootProperty = function ($point, propertyList, itemUsedNumList) {
             newValue = num + extraNum;
             newValue = Math.round(newValue / (newValue + 100) * 100);
             diffValue = newValue - propertyList.get('暴击几率');
-            unit = '%';
             break;
         case 'i1':
             newValue = num + extraNum;
             newValue = Math.round(newValue / (newValue + 120) * 100);
             diffValue = newValue - propertyList.get('技能释放概率');
-            unit = '%';
             break;
         case 'i2':
             newValue = num + extraNum;
             newValue = Math.round(newValue / (newValue + 150) * 100);
             diffValue = newValue - propertyList.get('防御');
-            unit = '%';
             break;
     }
-    if (num !== oriNum) {
-        $('#pd_new_' + name).html(
-            ` (<span style="color:#00F">${newValue}${unit}</span>|<span style="color:${diffValue >= 0 ? '#ff0033' : '#339933'}">` +
-            `${(diffValue >= 0 ? '+' : '') + diffValue}</span>)`
-        );
-    }
-    else {
-        $('#pd_new_' + name).html('');
-    }
+    $('#pdPro_' + name).text(newValue).css('color', num !== oriNum ? '#00f' : '#000');
+
+    if (num !== oriNum)
+        $('#pdNew_' + name).text(`(${(diffValue >= 0 ? '+' : '') + diffValue})`).css('color', diffValue >= 0 ? '#ff0033' : '#339933');
+    else
+        $('#pdNew_' + name).text('');
 };
 
 /**
  * 增强争夺记录
  */
 const enhanceLootLog = function () {
-    let $log = $('.kf_fw_ig1 > tbody > tr:nth-child(5) > td');
+    let $log = $lootArea.find('> tbody > tr:nth-child(5) > td');
     let matches = $log.html().match(/获得\d+经验和\d+KFB/g);
     let exp = 0, kfb = 0;
     for (let i in matches) {
