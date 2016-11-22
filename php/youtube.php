ce33b198a763ac0bf49aab09f1a67222c618cdeb
miaolapd <miaolapd@163.com>
2016-11-22 22:14:08 +0800

    增加了解析Youtube视频实际地址的PHP脚本

---
+++ php/youtube.php
@@ -0,0 +1,14 @@
+﻿<?php
+$id = $_GET['id'];
+$quality = $_GET['quality'];
+$content = file_get_contents('http://keepvid.com/?url=' . urlencode('https://www.youtube.com/watch?v=' . $id));
+$url = '';
+if ($quality != 'low' && preg_match('/<a href="([^"]+)"[^<>]+>[^<>]+<\/a>[^<>]+<b>(\(Max 720p\)|720p)<\/b>/i', $content, $matches)) {
+    $url = $matches[1];
+}
+else if (preg_match('/<a href="([^"]+)"[^<>]+>[^<>]+<\/a>[^<>]+<b>(\(Max 480p\)|480p)<\/b>/i', $content, $matches)) {
+    $url = $matches[1];
+}
+if ($url) Header("Location: $url");
+else echo 'Video No Found';
+?>
