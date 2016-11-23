d8aec2152db883af200a34bed16ed1e47ae09094
miaolapd <miaolapd@163.com>
2016-11-23 19:19:10 +0800

    【屏蔽首页神秘系数排名】自定义脚本更新V1.2

--- scripts/es5/BlockIndexSmRank.user.js
+++ scripts/es5/BlockIndexSmRank.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        屏蔽首页神秘系数排名
-// @version     1.1
+// @version     1.2
 // @trigger     end
 // @author      喵拉布丁
 // @homepage    read.php?tid=500968&spid=12708083
@@ -11,7 +11,7 @@
     var Info = _interopRequireDefault(require('./Info')).default;
     if (Info.isInHomePage) {
         var $smRank = $('a[href="kf_growup.php"]');
-        var matches = /神秘\d+级/.exec($smRank.text());
-        if (matches) $smRank.html(matches[0]);
+        var smLevel = parseInt($smRank.data('smLevel'));
+        if (smLevel) $smRank.html('\u795E\u79D8<b>' + smLevel + '</b>\u7EA7');
     }
 })();
--- scripts/es6/BlockIndexSmRank.user.js
+++ scripts/es6/BlockIndexSmRank.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        屏蔽首页神秘系数排名
-// @version     1.1
+// @version     1.2
 // @trigger     end
 // @author      喵拉布丁
 // @homepage    read.php?tid=500968&spid=12708083
@@ -10,7 +10,7 @@
     const Info = _interopRequireDefault(require('./Info')).default;
     if (Info.isInHomePage) {
         let $smRank = $('a[href="kf_growup.php"]');
-        let matches = /神秘\d+级/.exec($smRank.text());
-        if (matches) $smRank.html(matches[0]);
+        let smLevel = parseInt($smRank.data('smLevel'));
+        if (smLevel) $smRank.html(`神秘<b>${smLevel}</b>级`);
     }
 })();
