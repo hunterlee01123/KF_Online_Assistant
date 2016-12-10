a709ba3844ad67877c1bd0956bdb9cea42fa569b
miaolapd <miaolapd@163.com>
2016-12-11 00:53:19 +0800

    修正了解析Youtube视频地址的BUG

--- php/youtube.php
+++ php/youtube.php
@@ -3,10 +3,10 @@ $id = $_GET['id'];
 $quality = $_GET['quality'];
 $content = file_get_contents('http://keepvid.com/?url=' . urlencode('https://www.youtube.com/watch?v=' . $id));
 $url = '';
-if ($quality != 'low' && preg_match('/<a href="([^"]+)"[^<>]+>[^<>]+<\/a>[^<>]+<b>(\(Max 720p\)|720p)<\/b>/i', $content, $matches)) {
+if ($quality != 'low' && preg_match('/<a href="([^"]+)".+?MP4 - (\(Max 720p\)|720p)<\/span>/i', $content, $matches)) {
     $url = $matches[1];
 }
-else if (preg_match('/<a href="([^"]+)"[^<>]+>[^<>]+<\/a>[^<>]+<b>(\(Max 480p\)|480p)<\/b>/i', $content, $matches)) {
+else if (preg_match('/<a href="([^"]+)".+?MP4 - (\(Max 480p\)|480p)<\/span>/i', $content, $matches)) {
     $url = $matches[1];
 }
 if ($url) Header("Location: $url");
