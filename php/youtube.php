e881da4d438159de80dbd9f3f249a2047bfb8023
miaolapd <miaolapd@163.com>
2017-01-22 19:30:29 +0800

    修改了Youtube外链解析的代码

--- php/youtube.php
+++ php/youtube.php
@@ -9,6 +9,6 @@ if ($quality != 'low' && preg_match('/<a href="([^"]+)".+?MP4 - (\(Max 720p\)|72
 else if (preg_match('/<a href="([^"]+)".+?MP4 - (\(Max 480p\)|480p)<\/span>/i', $content, $matches)) {
     $url = $matches[1];
 }
-if ($url) Header("Location: $url");
+if ($url) Header('Location: ' . str_replace('http://', 'https://', $url));
 else echo 'Video No Found';
 ?>
